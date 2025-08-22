c7c6bb07359937d71b67455288abe62cda10af7b
Pradene <liam.pradene@gmail.com>
2025-08-22 19:53:43 +0200

    Adding libft as submodule

--- libft/src/string/ft_strlen.c
+++
@@ -1,8 +0,0 @@
-int	ft_strlen(const char *s) {
-	int	i = 0;
-	
-	while (s && s[i]) {
-		i++;
-	}
-	return (i);
-}
--- libft/src/string/ft_strtolower.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void ft_strtolower(char *s) {
-    int i = -1;
-    
-    while (s && s[++i]) {
-        s[i] = ft_tolower(s[i]);
-    }
-}
